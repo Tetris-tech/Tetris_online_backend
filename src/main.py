664a7d848cc7ae6ab70ba6e92adf7089986f629b
.leonyd.oblivious <85785403+leonneed08@users.noreply.github.com>
2024-10-26 13:01:34 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from src.api.user_routes import router as user_router  # Import the user router
 
 app = fastapi.FastAPI()
 
-app.include_router(hello_world)
+app.include_router(hello_world) #TODO
 app.include_router(user_router)  # Register the user-related routes
 
 if __name__ == "__main__":
