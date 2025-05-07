69133a201d1ade7adabfd62447977e148a0a7f13
Maxim Tetyushkin <maxim.tetyushkin@saritasa.com>
2025-05-07 18:01:59 +0700

    Remove unneccessary services from docker compose

--- tasks.py
+++ tasks.py
@@ -25,11 +25,6 @@ ns.configure(
             docker=saritasa_invocations.DockerSettings(
                 main_containers=(
                     "postgres",
-                    "redis",
-                    "minio",
-                    "minio-create-bucket",
-                    "mailpit",
-                    "nginx",
                 ),
             ),
             fastapi=saritasa_invocations.FastAPISettings(
@@ -37,10 +32,6 @@ ns.configure(
             ),
             alembic=saritasa_invocations.AlembicSettings(
                 migrations_folder="migrations/versions",
-            ),
-            celery=saritasa_invocations.CelerySettings(
-                app="config.celery.app",
-                scheduler="celery.beat.PersistentScheduler",
             ),
             pre_commit=saritasa_invocations.PreCommitSettings(
                 hooks=["pre-commit", "pre-push"],
